dd0d0c3d4828f3a1b2b00fe00d361034551e4f26
trokhin87 <trokhin87@gmail.com>
2025-03-16 15:41:42 +0300

    вроде все

---
+++ HttpClient/FriendHttpClient.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using DTO;
+
+namespace HttpClients
+{
+    public class FriendHttpClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public FriendHttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> AddFriendAsync(FriendDTO friendDto)
+        {
+            var response = await _httpClient.PostAsJsonAsync("add", friendDto);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> RemoveFriendAsync(RemoveFriendDTO removeFriendDto)
+        {
+            var response = await _httpClient.DeleteAsJsonAsync("delete", removeFriendDto);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> UpdateFriendAsync(FriendDTO friendDto)
+        {
+            var response = await _httpClient.PutAsJsonAsync("update", friendDto);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<List<FriendDTO>> GetFriendsByAppIdAsync(Guid appId)
+        {
+            return await _httpClient.GetFromJsonAsync<List<FriendDTO>>($"list/{appId}") ?? new List<FriendDTO>();
+        }
+
+        public async Task<WishIdDTO?> GetWishIdAsync(AppIDFriendDTO request)
+        {
+            return await _httpClient.GetFromJsonAsync<WishIdDTO?>($"pozdrik/{request.Username}/{request.AppID}");
+        }
+
+        public async Task<bool> AddWishAndInterestAsync(InterestAndWishDTO request)
+        {
+            var response = await _httpClient.PostAsJsonAsync("pozdrik/add", request);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
